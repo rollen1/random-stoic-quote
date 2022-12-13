71c7cccb8b850da23cfa354187f24208df339f8d
rollen1 <nolovedivision@gmail.com>
2022-12-13 20:09:38 +0200

    added more quotes

--- quotes.js
+++ quotes.js
@@ -3,5 +3,15 @@ const quotes = {
     2: 'Dwell on the beauty of life. Watch the stars, and see yourself running with them.',
     3: 'Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth.',
     4: 'Waste no more time arguing about what a good man should be. Be one.',
-    5: 'When you arise in the morning think of what a privilege it is to be alive, to think, to enjoy, to love ...'
+    5: 'When you arise in the morning think of what a privilege it is to be alive, to think, to enjoy, to love ...',
+    6: 'If you are distressed by anything external, the pain is not due to the thing itself, but to your estimate of it; and this you have the power to revoke at any moment.',
+    7: 'The best revenge is to be unlike him who performed the injury.',
+    8: 'It is not death that a man should fear, but he should fear never beginning to live.',
+    9: 'Whenever you are about to find fault with someone, ask yourself the following question: What fault of mine most nearly resembles the one I am about to criticize?',
+    10: 'If it is not right do not do it; if it is not true do not say it.',
+    11: 'What we do now echoes in eternity.',
+    12: 'Perfection of character is this: to live each day as if it were your last, without frenzy, without apathy, without pretence.',
+    13: `A persons worth is measured by the worth of what he values.`,
+    14: 'The things you think about determine the quality of your mind.',
+    15: 'It never ceases to amaze me: we all love ourselves more than other people, but care more about their opinion than our own.'
 }
